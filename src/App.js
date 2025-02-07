5af027b2820f8ad6f8f9e4634a76c4f8d8707b58
medban <bannourmohamedabdesslam@gmail.com>
2025-02-07 21:17:43 +0100

    third push

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+// import Catalogue from "./components/catalogue"
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      {/* <Catalogue/> */}
       <Projects />
       <Contact />
       <Footer />
