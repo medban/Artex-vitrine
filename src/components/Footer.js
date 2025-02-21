1d61918e8f15bb9d38ad6a2a88b25458e17977d3
medban <bannourmohamedabdesslam@gmail.com>
2025-02-21 18:49:42 +0100

    pages updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logowite.png";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -9,7 +8,7 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
+    
           <Col size={12} sm={6}>
           <a href="#home">  <img src={logo} alt="Logo" /> </a>
           </Col>
